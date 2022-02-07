0439289b30b1c3e6528f7ac2375eab0ab0e969b1
Bassam Kurdali <bassam@urchn.org>
2022-02-06 22:50:04 -0500

    Onion skin object creation theortically functional (Still not ready to test)

--- stop_motion/onion_skins.py
+++ stop_motion/onion_skins.py
@@ -36,35 +36,33 @@ def collection_name(source):
     return f"STPMO_onion_{source.name}"
 
 
-def create_onion(source, offset):
-    """ Create an onion skin object """
-    # get the onion collection
-    name = collection_name(source)
-    forward = offset >= 0
-    collections = bpy.data.collections
-    collection = collections.get(name, collections.new(name))
-    collection.use_fake_user = True
-    collection.use_select = False
-    collection.show_render = False
-    # Create the object
-    name = f"STPMO_onion{'+' if forward else '-'}_{offset:02}_{source.name}"
-    objects = bpy.data.objects
-    onion_object = objects.get(name, objects.new(name=name, object_data=source.data))
-    onion_object.use_select = False
-    onion_object.show_render = False
-    # put it in the collection
-    collection.objects.link(onion_object)
-    # Create the material
-    name = f"STPMO_onion_{'+' if forward else '-'}_mat"
+def object_name(forward, source, index):
+    return f"STPMO_onion{'+' if forward else '-'}_{index:02}_{source.name}"
+
+
+def material_name(forward):
+    return f"STPMO_onion_{'+' if forward else '-'}_mat"
+
+
+def create_material(forward)
+    name = material_name(forward)
     materials = bpy.data.materials
-    material = materials.get(name, materials.new(name))
+    if name in materials:
+        return
+
+    material = materials.get(name)
+    if material:
+        return material
+    material = materials.new(name)
     material.use_nodes = True
     for node in material.node_tree.nodes:
         if node.type != 'OUTPUT_MATERIAL':
             material.node_tree.nodes.remove(node)
     group_node = material.node_tree.nodes.new(type='ShaderNodeGroup')
-    onion_group = None # have to save and get it
+    filepath = os.path.join(os.path.dirname(__file__), "material.json")
+    onion_group = json_nodes.read_node("Oniony", filepath)
     group_node.node_tree = onion_group
+
     # Adjust Material Settings
     # Turn on Transparency, turn off shadows
     material.blend_method = 'BLEND'
@@ -73,6 +71,36 @@ def create_onion(source, offset):
     # Viewport display options
     material.diffuse_color = [1 , 0, 0, .2] if forward else [0, 1, .2, .2]
     material.roughness = 0
+
+
+def create_onion(source, offset, index):
+    """ Create an onion skin object """
+    forward = offset >= 0 # What direction am I in?
+
+    # get the onion collection
+    name = collection_name(source)
+    collections = bpy.data.collections
+    collection = collections.get(name, collections.new(name))
+    collection.use_fake_user = True
+    collection.hide_select = collection.hide_render = True
+    # Create the object
+    name = object_name(forward, source, index)
+    objects = bpy.data.objects
+    onion_object = objects.get(name)
+    if onion_object:
+        if onion_object.name not in collection.objects:
+            collection.objects.link(onion_object)
+        return onion_object
+    onion_object = objects.new(name=name, object_data=source.data))
+    onion_object.hide_select = onion_object.hide_render = True
+
+    # put it in the collection
+    if onion_object.name not in collection.objects:
+        collection.objects.link(onion_object)
+
+    # Create the material
+    material = create_material(forward)
+
     # add the modifiers
     for name, json_path, modname in (
             ("MeshKey", "modifier.json", Modifier.name),
@@ -82,7 +110,8 @@ def create_onion(source, offset):
         modifier = onion_object.modifiers.new(modname, 'NODES')
         modifier.node_group = node_group
     # Assign material to second modifier
-    modifier.material = material # pseudocode won't work
+    identifier = modifier.node_group.inputs['Material'].identifier
+    modifier[identifier] = material
     # Copy modifier settings from source
     target_modifier = Modifier(source)
     my_modifier = Modifier(onion_object)
@@ -90,7 +119,14 @@ def create_onion(source, offset):
     my_modifier.index = target_modifier.index + offset
 
     # Adjust object viewport properties to match material
+
     # do the nla of the action, offset it by offset
+    action = source.animation_data.action
+    animation_data = onion_object.animation_data_create()
+    nla_track = animation_data.nla_tracks.new()
+    strip = nla_track.strips.new(
+        action.name, action.frame_range[0] + offset, action)
+
     return onion_object
 
 
